559114d4f942e9329211f862613dd9e6111dc095
sapslaj <saps.laj@gmail.com>
2015-10-22 11:36:03 -0400

    yeah buddy

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('codeLab', []);
+
+app.controller('RepoController', function ($scope, $http) {
+  $scope.loaded = false;
+
+  $scope.die = function (message) {
+    alert(message);
+  }
+
+  $http({
+    method: 'GET',
+    url: 'https://api.github.com/orgs/layer8x/repos'
+  })
+    .then(function successCallback(response) {
+      $scope.repos = response.data;
+      addPagesUrlToRepos();
+      $scope.loaded = true;
+    }, function errorCallback(response) {
+      die(response);
+    });
+
+  function addPagesUrlToRepos() {
+    $scope.repos.forEach(function (repo, index, repos) {
+      $http.get('https://raw.githubusercontent.com/layer8x/' + repo.name + '/master/CNAME')
+        .then(function successCallback(response) {
+          repo.pages_url = 'http://' + response.data;
+        }, function errorCallback(response) {
+          $http.get('https://raw.githubusercontent.com/layer8x/' + repo.name + '/gh-pages/CNAME')
+          .then(function successCallback(response) {
+            repo.pages_url = 'http://' + response.data;
+          }, function errorCallback(response) {
+            repo.pages_url = "https://layer8x.github.io/" + repo.name;
+          });
+        });
+    });
+  };
+});
