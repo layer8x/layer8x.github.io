da92b461253e0bf21e86e24de6e08c5b46f93afe
sapslaj <saps.laj@gmail.com>
2015-10-24 23:44:29 -0400

    "http://layer8x.github.io/layer8x.github.io" is not a thing you can do.

--- js/app.js
+++ js/app.js
@@ -21,17 +21,21 @@ app.controller('RepoController', function ($scope, $http) {
 
   function addPagesUrlToRepos() {
     $scope.repos.forEach(function (repo, index, repos) {
-      $http.get('https://raw.githubusercontent.com/layer8x/' + repo.name + '/master/CNAME')
-        .then(function successCallback(response) {
-          repo.pages_url = 'http://' + response.data;
-        }, function errorCallback(response) {
-          $http.get('https://raw.githubusercontent.com/layer8x/' + repo.name + '/gh-pages/CNAME')
+      if (repo.name.indexOf('github.io') == -1) {
+        $http.get('https://raw.githubusercontent.com/layer8x/' + repo.name + '/master/CNAME')
           .then(function successCallback(response) {
             repo.pages_url = 'http://' + response.data;
           }, function errorCallback(response) {
-            repo.pages_url = "https://layer8x.github.io/" + repo.name;
-          });
-        });
+            $http.get('https://raw.githubusercontent.com/layer8x/' + repo.name + '/gh-pages/CNAME')
+              .then(function successCallback(response) {
+                repo.pages_url = 'http://' + response.data;
+              }, function errorCallback(response) {
+                repo.pages_url = "https://layer8x.github.io/" + repo.name;
+              });
+            });
+      } else {
+        repo.pages_url = 'http://' + repo.name;
+      };
     });
   };
 });
